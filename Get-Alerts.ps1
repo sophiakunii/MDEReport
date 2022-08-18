36ae797266b2c735e3a4f8bface7ea6bd956c6f9
Kunii, Suguru <87286326+sophiakunii@users.noreply.github.com>
2022-08-18 09:24:43 +0900

    Add files via upload

---
+++ Get-Alerts.ps1
@@ -0,0 +1,40 @@
+﻿# Returns Alerts created in the past 48 hours.
+
+# Parameters
+$tenantId = 'ddb7f0e6-e7aa-4ad6-b331-7fba86375b4c'
+$appId = '13efcb20-3ac9-4d34-ab09-0a648c6bc44f'
+$appSecret = 'Cau8Q~kocu6ECXDkPadTxlD5TlAk-FBrTZwJQb_N'
+
+# Get Token from Azure AD.
+$resourceAppIdUri = 'https://api.securitycenter.windows.com'
+$oAuthUri = "https://login.windows.net/$TenantId/oauth2/token"
+
+$authBody = [Ordered] @{
+    resource = "$resourceAppIdUri"
+    client_id = "$appId"
+    client_secret = "$appSecret"
+    grant_type = 'client_credentials'
+}
+
+$authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+$token = $authResponse.access_token
+
+# Get Alert from the last 48 hours. Make sure you have alerts in that time frame.
+$dateTime = (Get-Date).ToUniversalTime().AddHours(-48).ToString("o")       
+
+# The URL contains the type of query and the time filter we create above
+# Read more about other query options and filters at   Https://TBD- add the documentation link
+$url = "https://api.securitycenter.windows.com/api/alerts?`$filter=alertCreationTime ge $dateTime"
+
+# Set the WebRequest headers
+$headers = @{ 
+    'Content-Type' = 'application/json'
+    Accept = 'application/json'
+    Authorization = "Bearer $token" 
+}
+
+# Send the webrequest and get the results. 
+$response = Invoke-WebRequest -Method Get -Uri $url -Headers $headers -ErrorAction Stop
+
+# Extract the alerts from the results. 
+($response | ConvertFrom-Json).value | ConvertTo-Json
